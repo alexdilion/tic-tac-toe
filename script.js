f9fb7f2f1cc85872d1c456f826af47fa41d719c0
alexdilion <alexdilion@protonmail.com>
2023-05-15 14:56:09 +0100

    Refactor code

--- script.js
+++ script.js
@@ -2,44 +2,46 @@ const DISPLAY = document.querySelector("#board");
 
 // Game board module
 const gameBoard = (() => {
-    let boardArray = ["x", "x", "x", "o", "", "o", "x", "", ""];
+    let boardArray = ["1", "2", "", "1", "", "1", "2", "", ""];
 
-    const setTile = (position, mark) => {
-        boardArray[position.x][position.y] = mark;
+    const setTile = (index, playerNumber) => {
+        boardArray[index] = playerNumber;
     };
 
-    const isValidMove = (position) => boardArray[position.x][position.y] === "";
+    const isValidMove = (index) => boardArray[index] === "";
     const getBoard = () => boardArray;
 
-    const clearGrid = () => {
+    const clear = () => {
         boardArray = ["", "", "", "", "", "", "", "", ""];
     };
 
     return {
         setTile,
-        clearGrid,
+        clear,
         isValidMove,
         getBoard,
     };
 })();
 
 const displayController = ((display) => {
-    const cross = document.querySelector(".hidden-elements .cross");
-    const circle = document.querySelector(".hidden-elements .circle");
-
     const updateDisplay = () => {
         const boardArray = gameBoard.getBoard();
+        const p1Mark = "close"; // placeholder, will get mark later
+        const p2Mark = "circle"; // placeholder, will get mark later
 
-        boardArray.forEach((mark, index) => {
-            if (mark === "") return;
-
-            const button = display.querySelector(`button[data-index='${index}']`);
-            button.innerHTML = "";
-
-            if (mark === "x") {
-                button.appendChild(cross.cloneNode(true));
-            } else if (mark === "o") {
-                button.appendChild(circle.cloneNode(true));
+        boardArray.forEach((tile, index) => {
+            const htmlMark = display.querySelector(`button[data-index='${index}'] > span`);
+            
+            if (tile === "") {
+                htmlMark.classList.add("transparent")
+            } else if (tile === "1") {
+                htmlMark.textContent = p1Mark;
+                htmlMark.classList.remove("player2", "transparent");
+                htmlMark.classList.add("player1")
+            } else if (tile === "2") {
+                htmlMark.textContent = p2Mark;
+                htmlMark.classList.remove("player1", "transparent");
+                htmlMark.classList.add("player2")
             }
         });
     };
@@ -50,20 +52,34 @@ const displayController = ((display) => {
 })(DISPLAY);
 
 const Player = (name, mark) => {
-    const takeTurn = (position) => {
-        if (!gameBoard.isValidMove(position)) {
+    let playerName = name;
+    let playerMark = mark;
+
+    const placeMark = (index) => {
+        if (!gameBoard.isValidMove(index)) {
+            console.log(gameBoard.isValidMove(index));
             return;
         }
 
-        gameBoard.setTile(position, mark);
+        gameBoard.setTile(index, mark);
     };
 
-    const getName = () => name;
-    const getMark = () => mark;
+    const getName = () => playerName;
+    const getMark = () => playerMark;
+    
+    const setName = (newName) => {
+        playerName = newName;
+    }
+
+    const setMark = (newMark) => {
+        playerMark = newMark;
+    }
 
     return {
-        takeTurn,
+        placeMark,
         getName,
         getMark,
+        setName,
+        setMark
     };
 };
